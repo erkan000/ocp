b8856498ea689d026005c32b30d05d5cd52f1532
Erkan <erkan000@hotmail.com>
2021-03-30 11:52:35 +0300

    .

---
+++ src/main/java/chapter8/s3/ByteBufferTest.java
@@ -0,0 +1,50 @@
+package chapter8.s3;
+
+import java.nio.*;
+
+public class ByteBufferTest {
+	
+	/**
+	 * 
+	 * 
+- hasRemaining() yordamı, mevcut pozisyon ile limit arasındaki eleman sayısını bildirir.
+- flip() yordamı çağrıldığında, ByteBuffernesnesinde tanımlı olan pozisyonundeğeri sıfıra eşitlenir. Limitdeğeri okunamayacak veya yazılamayacak olan son elemanın üzerine getirilir ve biraz ileride göreceğimiz
+- mark() yordamının kullanılmasıyla işaretlenmiş olan yer silinir.
+- get() yordamı, belirtilen elemanı okur.
+	 * 
+	 * 
+	 */
+
+	public static void main(String args[]) {
+
+		ByteBuffer bb =  ByteBuffer.allocate(10);
+		bb.put( (byte)'a' );
+		bb.put( (byte)'b' );
+		bb.put( (byte)'c' );
+		bb.putInt(5);
+
+		int kapasite = bb.capacity();
+		int limit = bb.limit();
+		int pozisyon = bb.position();
+
+		System.out.println(" kapasite:"+kapasite);
+		System.out.println(" limit:"+limit);
+		System.out.println(" pozisyon:"+pozisyon);
+		System.out.println("-------flip()--------------");
+
+		bb.flip(); // onemli 
+
+		kapasite = bb.capacity();
+		limit = bb.limit();
+		pozisyon = bb.position();
+
+		System.out.println(" kapasite:"+kapasite);
+		System.out.println(" limit:"+limit);
+		System.out.println(" pozisyon:"+pozisyon);
+		System.out.println("---------------------------");
+
+		while (bb.hasRemaining()) {
+			System.out.println( "-->" + bb.get() );
+		}
+	}
+}
