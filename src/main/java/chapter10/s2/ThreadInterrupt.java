7f81bc54777d065a009e95c5a6d5001008d61701
Erkan <erkan000@hotmail.com>
2024-07-19 17:49:59 +0300

    Thread interrputr

---
+++ src/main/java/chapter10/s2/ThreadInterrupt.java
@@ -0,0 +1,25 @@
+package chapter10.s2;
+
+public class ThreadInterrupt {
+
+	public static void main(String[] args) {
+		Thread thread = new Thread(new BlockingTask());
+
+		thread.start();
+
+		thread.interrupt();
+	}
+
+	private static class BlockingTask implements Runnable {
+
+		@Override
+		public void run() {
+			try {
+				Thread.sleep(500000);
+			} catch (InterruptedException e) {
+				System.out.println("Existing blocking thread");
+			}
+		}
+	}
+
+}
